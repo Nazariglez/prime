7ca29746def0d617cda8621ef5bc9f84911e273f
Nazarí González <nazari.nz@gmail.com>
2016-12-31 20:26:03 +0000

    moved the WebGL attributes from base context to the WebGL context to fix a weird bug (just for now)

--- gfx/gl/gl.go
+++ gfx/gl/gl.go
@@ -1,13 +1,9 @@
-/**
- * Created by nazarigonzalez on 30/12/16.
- */
+// Created by nazarigonzalez on 30/12/16.
 
 //GL Wrapper (OpenGL 2.1, WebGL 1.0 and OpenGL ES 2.0)
+// Forked -> https://github.com/ajhager/webgl/
 
 package gl
-
-//wrapper of OpenGL 2.1, WebGL 1.0 and OpenGL ES 2.0
-// -> https://github.com/ajhager/webgl/
 
 type BaseContext struct {
   ARRAY_BUFFER                                 int `js:"ARRAY_BUFFER"`
