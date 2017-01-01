9e0f0c2862b0363440abcece8a8a68a0c976d4ea
Nazarí González <nazari.nz@gmail.com>
2017-01-01 23:15:10 +0000

    minor refactor

--- gfx/gfx.go
+++ gfx/gfx.go
@@ -3,11 +3,10 @@
 package gfx
 
 import (
-  _"runtime"
   "errors"
+  "log"
 
   "prime/gfx/gl"
-  "log"
 )
 
 var (
@@ -16,30 +15,16 @@ var (
   gfxWidth int
   gfxHeight int
   gfxTitle string
-  gfxBg []float32
 
   OnStart = func(){ log.Println("GFX Event: Start") }
   OnEnd = func(){ log.Println("GFX Event: End") }
   OnDraw = func(){ log.Println("GFX Event: Draw") }
 )
 
-type GfxEvent int
-const (
-  GFX_START GfxEvent = iota
-  GFX_DRAW
-  GFX_END
-)
-
-var gfxChannel chan GfxEvent
-
 func Init(width, height int, title string, bg []float32) error {
   gfxWidth = width
   gfxHeight = height
   gfxTitle = title
-  gfxBg = bg
-
-  gfxChannel = make(chan GfxEvent)
-  //go readEvents()
   return initialize()
 }
 
@@ -49,17 +34,4 @@ func GetContext() (*gl.Context, error) {
   }
 
   return nil, errors.New("Gfx needs start before get the context.")
-}
-
-func readEvents() {
-  for e := range gfxChannel {
-    switch e {
-    case GFX_START:
-      OnStart()
-    case GFX_DRAW:
-      OnDraw()
-    case GFX_END:
-      OnEnd()
-    }
-  }
 }
