1189b56aa56fac5cabf7c42e1a333b200a3edf85
Nazarí González <nazari.nz@gmail.com>
2017-01-15 22:14:16 +0000

    added a simple test actor

---
+++ Actor.go
@@ -0,0 +1,45 @@
+// Created by nazarigonzalez on 15/1/17.
+
+package prime
+
+import "prime/gfx"
+
+//todo actor pool
+
+type Actor struct {
+	texture  *gfx.Texture
+	bounds   *Rect
+	position *Point
+	scale    *Point
+}
+
+func (a *Actor) Texture() *gfx.Texture {
+	return a.texture
+}
+
+func (a *Actor) Bounds() *Rect {
+	return a.bounds
+}
+
+func (a *Actor) Position() *Point {
+	return a.position
+}
+
+func (a *Actor) Scale() *Point {
+	return a.scale
+}
+
+type DisplayObject interface {
+	Texture() *gfx.Texture
+	Bounds() *Rect
+	Scale() *Point
+	Position() *Point
+}
+
+type Rect struct {
+	X, Y, Width, Height int
+}
+
+type Point struct {
+	X, Y int
+}
