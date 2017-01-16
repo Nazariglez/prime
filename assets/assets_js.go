20757a8fc2066b22e7e09220aa9d176017cd18bd
Nazarí González <nazari.nz@gmail.com>
2017-01-16 23:59:57 +0000

    minor fix

--- assets/assets_js.go
+++ assets/assets_js.go
@@ -24,13 +24,16 @@ func LoadImage(imgFile string) (*gfx.Image, error) {
 		return nil, errors.New("Unable to load the image: " + imgFile)
 	}
 
+  w := make(chan bool, 1)
 	img := js.Global.Get("document").Call("createElement", "img")
 	img.Set("onload", func() {
 		js.Global.Get("window").Get("URL").Call("revokeObjectURL", img.Get("src"))
+    w <- true
 	})
 
 	img.Set("src", js.Global.Get("window").Get("URL").Call("createObjectURL", req.Response))
 
+  <-w
 	js.Global.Get("document").Get("body").Call("appendChild", img)
 	return gfx.NewImage(img), nil
 }
